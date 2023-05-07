212ad9ccc944cc7db7271a60c6f0054b5187a8cb
Grzegorz Bartoszek <grze.bartoszek@gmail.com>
2023-05-07 09:35:41 +0100

    Add heuristic

--- src/maze.rs
+++ src/maze.rs
@@ -1,39 +1,31 @@
+use std::error::Error;
+
 pub fn solve(input: Maze) -> Option<usize> {
     let start_pos = (0, 1);
-    let goal = (input[0].len() - 1, input.len() - 2);
+    let goal = (input.max_x(), input.max_y() - 1);
 
-    let mut open = std::collections::HashMap::<(usize, usize), (usize, Option<Direction>)>::new();
+    let mut open =
+        std::collections::HashMap::<(usize, usize), (usize, usize, Option<Direction>)>::new();
     let mut closed = std::collections::HashMap::<(usize, usize), usize>::new();
 
-    open.insert(start_pos, (0, None));
+    open.insert(start_pos, (0, 0, None));
 
     loop {
-        println!("OPEN: {open:?}");
-        println!("CLOSED: {closed:?}");
-
         let next = open
             .iter()
-            .min_by(|l, r| l.1.0.cmp(&r.1.0))
-            .map(|(p, (f, d))| (*p, (*f, *d)));
+            .min_by(|l, r| l.1 .0.cmp(&r.1 .0))
+            .map(|(p, (f, g, d))| (*p, (*f, *g, *d)));
 
+        println!("NEXT: {:?}", next);
 
-        println!("NEXT: {next:?}");
-
-        if let Some((pos, (v,d))) = next {
+        if let Some((pos, (of, og, d))) = next {
             if pos == goal {
-                return Some(v);
+                return Some(og);
             }
 
             for n in neighbours(pos, &input) {
-
-                let g: usize = if d.is_none() || Some(n.1) == d {
-                    v
-                } else {
-                    v + 1
-                };
-
-                let h = 0;
-
+                let g = g(d, n.1, og);
+                let h = h(n.1, n.0, goal);
                 let f = g + h;
 
                 match closed.get(&n.0) {
@@ -42,26 +34,78 @@ pub fn solve(input: Maze) -> Option<usize> {
                 }
 
                 match open.get(&n.0) {
-                    Some((o, _)) if *o < f => continue,
+                    Some((o, _, _)) if *o < f => continue,
                     _ => {}
                 }
 
-                println!("N: {:?} {:?}", n.0, n.1);
-                open.insert(n.0, (f, Some(n.1)));
+                println!("Adding {:?} with f {}", n.1, f);
+
+                open.insert(n.0, (f, g, Some(n.1)));
             }
 
             open.remove(&pos);
-            closed.insert(pos, v);
+            closed.insert(pos, of);
         } else {
             return None;
         }
     }
+}
 
-    Some(0)
+// A* "g" function determining the cost of moving to the current spot.
+// It does it by comparing the previous direction on the path with the one required
+// to move to the currently examined neighbour
+fn g(
+    previous_direction: Option<Direction>,
+    neighbour_direction: Direction,
+    corners_so_far: usize,
+) -> usize {
+    match previous_direction {
+        Some(p) if p != neighbour_direction => corners_so_far + 1,
+        _ => corners_so_far,
+    }
+}
+
+// A* "h" - a heuristic trying to guess the remaining cost to get to the goal
+// This version does it by taking the minimal number of corners required to get to the goal assuming there are no walls in the maze
+fn h(d: Direction, (pos_x, pos_y): Pos, (goal_x, goal_y): Pos) -> usize {
+    match (d, pos_x, pos_y) {
+        (Direction::Right, _, y) if y == goal_y => 0,
+        (Direction::Right, _, y) if y != goal_y => 1,
+
+        (Direction::Up, x, y) if x == goal_x && y > goal_y => 0,
+        (Direction::Up, x, y) if x == goal_x && y < goal_y => 3,
+        (Direction::Up, x, y) if x != goal_x && y > goal_y => 1,
+        (Direction::Up, x, y) if x != goal_x && y < goal_y => 3,
+        (Direction::Up, x, y) if x != goal_x && y == goal_y => 2,
+
+        (Direction::Down, x, y) if x == goal_x && y < goal_y => 0,
+        (Direction::Down, x, y) if x == goal_x && y > goal_y => 3,
+        (Direction::Down, x, y) if x != goal_x && y < goal_y => 1,
+        (Direction::Down, x, y) if x != goal_x && y > goal_y => 3,
+        (Direction::Down, x, y) if x != goal_x && y == goal_y => 2,
+
+        (Direction::Left, _, y) if y == goal_y => 3,
+        _ => 2,
+    }
 }
 
 type Maze = Vec<Vec<bool>>;
 type Pos = (usize, usize);
+
+trait MazeExt {
+    fn max_y(&self) -> usize;
+    fn max_x(&self) -> usize;
+}
+
+impl MazeExt for Maze {
+    fn max_y(&self) -> usize {
+        self.len() - 1
+    }
+
+    fn max_x(&self) -> usize {
+        self[0].len() - 1
+    }
+}
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum Direction {
@@ -81,11 +125,11 @@ fn neighbours((x, y): Pos, maze: &Maze) -> impl Iterator<Item = (Pos, Direction)
     .into_iter()
     .filter_map(move |d| match d {
         Direction::Up if y > 0 && maze[y - 1][x] => Some(((x, y - 1), Direction::Up)),
-        Direction::Down if y < maze.len() - 1 && maze[y + 1][x] => {
+        Direction::Down if y < maze.max_y() && maze[y + 1][x] => {
             Some(((x, y + 1), Direction::Down))
         }
         Direction::Left if x > 0 && maze[y][x - 1] => Some(((x - 1, y), Direction::Left)),
-        Direction::Right if x < maze[0].len() - 1 && maze[y][x + 1] => {
+        Direction::Right if x < maze.max_x() && maze[y][x + 1] => {
             Some(((x + 1, y), Direction::Right))
         }
         _ => None,
@@ -114,5 +158,61 @@ mod tests {
                            000000000";
 
         assert_eq!(solve(to_input(input)), Some(4));
+    }
+
+    #[test]
+    fn no_solution() {
+        let input = "000000000
+                           ....0...0
+                           0.0.0.0.0
+                           0.0...0.0
+                           0.00000.0
+                           0....00.0
+                           0.00...0.
+                           000000000";
+
+        assert_eq!(solve(to_input(input)), None);
+    }
+
+    #[test]
+    fn no_walls() {
+        let input = "000000000
+                           .00000000
+                           .00000000
+                           .00000000
+                           .00000000
+                           .00000000
+                           .0000000.
+                           .........";
+
+        assert_eq!(solve(to_input(input)), Some(2));
+    }
+
+    #[test]
+    fn no_corner_on_first_step() {
+        let input = "000000000
+                           ........0
+                           .000000.0
+                           .000000.0
+                           .000000.0
+                           .000000.0
+                           .........
+                           000000000";
+
+        assert_eq!(solve(to_input(input)), Some(1));
+    }
+
+    #[test]
+    fn no_maze() {
+        let input = ".........
+                           .........
+                           .........
+                           .........
+                           .........
+                           .........
+                           .........
+                           .........";
+
+        assert_eq!(solve(to_input(input)), Some(1));
     }
 }
